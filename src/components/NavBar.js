139eaec8c172de31c7c8cb1f1f062326242187a7
Kattis91 <katya.svedmark@gmail.com>
2023-11-10 21:33:32 +0000

    Add navlinks to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="75" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="75" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link><i class="fa-solid fa-house-user"></i> Home</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i> Sign in</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
+            <NavLink to="/">
+              <i class="fa-solid fa-house-user"></i>Home
+            </NavLink>
+            <NavLink to="/signin">
+              <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink to="/signup">
+              <i class="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
