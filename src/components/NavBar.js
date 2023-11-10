456f8ff970caec21d992e7888bd8d4394604ee95
Kattis91 <katya.svedmark@gmail.com>
2023-11-10 22:40:55 +0000

    Add style to navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,11 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -17,13 +18,26 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <NavLink to="/">
+            <NavLink 
+              exact
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to="/"
+            >
               <i class="fa-solid fa-house-user"></i>Home
             </NavLink>
-            <NavLink to="/signin">
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to="/signin"
+            >
               <i class="fa-solid fa-right-to-bracket"></i>Sign in
             </NavLink>
-            <NavLink to="/signup">
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to="/signup"
+            >
               <i class="fa-solid fa-user-plus"></i>Sign up
             </NavLink>
           </Nav>
