c4d61ff5ac629bd6aa8925ad0fc44d27de1ee2e3
Kattis91 <katya.svedmark@gmail.com>
2023-11-10 19:37:57 +0000

    Create App.module.css and add basic style to app and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import styles from "./App.module.css";
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home page</h1>
+        <h1>Sign in</h1>
+      </Container>
     </div>
   );
 }
