88a500e2ee7b5e8730b302d0369a53d239bc494d
Kattis91 <katya.svedmark@gmail.com>
2023-11-11 19:50:53 +0000

    Update navbar to display different icons depending on login status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
 import styles from "../styles/NavBar.module.css";
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink 
+        className={styles.NavLink} 
+        activeClassName={styles.Active} 
+        to="/signin"
+      >
+        <i className="fa-solid fa-right-to-bracket"></i>Sign in
+      </NavLink>
+      <NavLink 
+        className={styles.NavLink} 
+        activeClassName={styles.Active} 
+        to="/signup"
+      >
+        <i className="fa-solid fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -26,20 +50,8 @@ const NavBar = () => {
             >
               <i class="fa-solid fa-house-user"></i>Home
             </NavLink>
-            <NavLink 
-              className={styles.NavLink} 
-              activeClassName={styles.Active} 
-              to="/signin"
-            >
-              <i class="fa-solid fa-right-to-bracket"></i>Sign in
-            </NavLink>
-            <NavLink 
-              className={styles.NavLink} 
-              activeClassName={styles.Active} 
-              to="/signup"
-            >
-              <i class="fa-solid fa-user-plus"></i>Sign up
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
