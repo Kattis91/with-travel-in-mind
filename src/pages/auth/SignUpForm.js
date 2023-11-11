fd711e7be9eecf360f9ce24692150ef75710a74b
Kattis91 <katya.svedmark@gmail.com>
2023-11-11 13:02:21 +0000

    Add more style to the sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,49 +1,64 @@
 import React from 'react'
-import { Button, Col, Form, Image, Row } from 'react-bootstrap'
+import { Button, Col, Container, Form, Image, Row } from 'react-bootstrap'
+
 import appStyles from "../../App.module.css";
+import styles from "../../styles/SignUpForm.module.css";
+import { Link } from 'react-router-dom';
 
 const SignUpForm = () => {
   return (
-    <Row>
-      <Col md={6}>
-        <h1>Sign Up</h1>
-        <Form>
-          <Form.Group controlId="username">
-            <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control 
-                type="text" 
-                placeholder="Enter username" 
-                name="username"
-              />
-          </Form.Group>
-          <Form.Group controlId="password1">
-            <Form.Label className="d-none">Password</Form.Label>
-            <Form.Control           
-              type="password" 
-              placeholder="Password"
-              name="password1"
-            />
-          </Form.Group>
-          <Form.Group controlId="password2">
-            <Form.Label className="d-none">Confirm password</Form.Label>
-            <Form.Control          
-              type="password" 
-              placeholder="Confirm password"
-              name="password2"
-            />
-          </Form.Group>
-          <Button type="submit">
-            Sign up
-          </Button>
-        </Form>
-      </Col>  
-      <Col md={6}>
+    <Row className={styles.Row}>
+      <Col md={6}
+        className={`my-auto d-none d-md-block ${styles.SignUpCol}`}
+      >
         <Image 
           className={`${appStyles.FillerImage}`}
           src={
           "https://res.cloudinary.com/dx0imlozl/image/upload/v1699661594/sign-up-form-image_j0wu6f.avif"}         
         />
       </Col>
+      <Col className="my-auto p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>Sign Up</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+                <Form.Control 
+                  className={styles.Input}
+                  type="text" 
+                  placeholder="Enter username" 
+                  name="username"
+                />
+            </Form.Group>
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control   
+                className={styles.Input}        
+                type="password" 
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control 
+                className={styles.Input}         
+                type="password" 
+                placeholder="Confirm password"
+                name="password2"
+              />
+            </Form.Group>
+            <Button type="submit">
+              Sign up
+            </Button>
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>  
     </Row>  
   )
 }
