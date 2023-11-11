6d4d8427944c891d40c8dd8ddbe216e454c7122a
Kattis91 <katya.svedmark@gmail.com>
2023-11-11 16:03:40 +0000

    Add style to SignInForm to match with SignUpForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,32 +1,57 @@
 import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Col, Container, Form, Image, Row } from 'react-bootstrap'
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/SignUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { Link } from 'react-router-dom';
 
 const SignInForm = () => {
   return (
-    <div>
-    <h1>Sign In</h1>
-    <Form>
-      <Form.Group controlId="username">
-        <Form.Label className="d-none">Username</Form.Label>
-          <Form.Control 
-            type="text" 
-            placeholder="Enter username" 
-            name="username"
-          />
-      </Form.Group>
-      <Form.Group controlId="password1">
-        <Form.Label className="d-none">Password</Form.Label>
-        <Form.Control           
-          type="password" 
-          placeholder="Password"
-          name="password1"
+    <Row className={styles.Row}>
+      <Col md={6}
+        className={`my-auto d-none d-md-block ${styles.SignInCol}`}
+      >
+        <Image 
+          className={`${appStyles.FillerImage}`}
+          src={
+            "https://res.cloudinary.com/dx0imlozl/image/upload/v1699661594/sign-up-form-image_j0wu6f.avif"}         
         />
-      </Form.Group>
-      <Button type="submit">
-        Sign in
-      </Button>
-    </Form>
-    </div>
+      </Col>
+      <Col className="my-auto p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>Sign In</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control 
+                className={styles.Input}
+                type="text" 
+                placeholder="Enter username" 
+                name="username"
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control   
+                className={styles.Input}        
+                type="password" 
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+            <Button className={`${btnStyles.Button} ${btnStyles.Bright}`} type="submit">
+              Sign in
+            </Button>
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
+            Don't have an account yet? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>  
+    </Row>  
   )
 }
 
