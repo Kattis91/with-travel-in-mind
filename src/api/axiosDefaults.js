ef0e1966050eaac45f48c6fcd32026058dc3f056
Kattis91 <katya.svedmark@gmail.com>
2023-11-12 12:21:15 +0000

    Set up axios interceptors to refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://with-travel-in-mind-api-a2180400f610.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://with-travel-in-mind-api-a2180400f610.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
