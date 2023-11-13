c75a1ca13b0a40a59b317c697091529a33e03fe4
Kattis91 <katya.svedmark@gmail.com>
2023-11-13 14:44:50 +0000

    Add the logic to get the data about a single post

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router";
 
 function PostPage() {
+
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+  
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    handleMount();
+  }, [id]);
  
-    return (
+  return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Most adventurous explorers for mobile</p>
