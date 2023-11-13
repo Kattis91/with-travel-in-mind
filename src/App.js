32bc6ff4601edbb66671e0f0a260459f7323c86d
Kattis91 <katya.svedmark@gmail.com>
2023-11-13 22:49:54 +0000

    Add Routes for the home, bookmarks, favorites and following pages

--- src/App.js
+++ src/App.js
@@ -7,15 +7,56 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostDetail from "./pages/posts/PostDetail";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+
+  const currentUser = useCurrentUser();
+  const explorer_id = currentUser?.explorer_id || "";
 
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword.."/>
+            )} 
+          />
+          <Route 
+            exact 
+            path="/bookmarks" 
+            render={() => (
+              <PostsPage 
+                message="No results found. Adjust the search keyword or bookmark a post.."
+                filter={`bookmarks__owner__explorer=${explorer_id}&ordering=-bookmarks__created_at&`}
+              />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/favorites" 
+            render={() => (
+              <PostsPage 
+                message="No results found. Adjust the search keyword or add a user into your favorite list.."
+                filter={`owner__favorited__owner__explorer=${explorer_id}&`}
+              />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/following" 
+            render={() => (
+              <PostsPage 
+                message="No results found. Adjust the search keyword or follow a user.."
+                filter={`owner__followed__owner__explorer=${explorer_id}&`}
+              />
+            )} 
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
