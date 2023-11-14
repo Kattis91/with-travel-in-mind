da13338a477026a9d9833f321bca98d27fe65a00
Kattis91 <katya.svedmark@gmail.com>
2023-11-14 12:37:21 +0000

    Add nested ternary to display posts when loaded

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
+import Post from "./Post";
+import Asset from "../../components/Asset";
+
+import appStyles from "../../App.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -30,7 +37,23 @@ function PostsPage({ message, filter = "" }) {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Most adventurous explorers for mobile</p>
-        <p>The list of posts goes here</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Most adventurous explorers for desktop</p>
