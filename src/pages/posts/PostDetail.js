b32221774b0444260ca8408e594c38af7331ccb4
Kattis91 <katya.svedmark@gmail.com>
2023-11-15 17:21:16 +0000

    Add API request.Add ternar to map through comments/check the current user status

--- src/pages/posts/PostDetail.js
+++ src/pages/posts/PostDetail.js
@@ -24,11 +24,12 @@ function PostDetail() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -55,6 +56,17 @@ function PostDetail() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <p key={comment.id}>
+                {comment.owner} : {comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>There are no comments for this post yet. Be the first to comment!</span>
+          ) : (
+            <span>No comments... yet. Log in to leave a comment!</span>
+          )}
   
         </Container>
       </Col>
