d73d6c432ee135fb71a811229a428c800d2784c4
Kattis91 <katya.svedmark@gmail.com>
2023-11-15 16:24:34 +0000

    Create a form to enable logged-in user to leave comments

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import btnStyles from "../../styles/Button.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+  const { post, setPost, setComments, explorerImage, explorer_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/explorers/${explorer_id}`}>
+            <Avatar src={explorerImage} />
+          </Link>
+          <Form.Control
+            placeholder="Write your comment here.."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${btnStyles.FormGreen} btn d-block ml-auto`}
+        type="submit"
+      >
+        Post
+      </button>
+    </Form>
+  );
+}
+
+export default CommentCreateForm;
