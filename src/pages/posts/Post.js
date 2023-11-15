f7ec3ba1b8020610681530f547840329726cbbdb
Kattis91 <katya.svedmark@gmail.com>
2023-11-15 12:29:11 +0000

    Add the logic for the modal to be triggered when clicking on the delete icon

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,7 @@ import styles from '../../styles/Post.module.css'
 import { axiosRes } from '../../api/axiosDefaults';
 import { EditDeleteDropdown } from '../../components/EditDeleteDropdown';
 import { useHistory } from 'react-router';
+import ModalDeleteConfirmation from '../../components/ModalDeleteConfirmation';
 
 const Post = (props) => {
 
@@ -33,6 +34,11 @@ const Post = (props) => {
   const is_owner = currentUser?.username === owner;
 
   const history = useHistory();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleEdit = () => {
     history.push(`/posts/${id}/edit`);
@@ -80,84 +86,91 @@ const Post = (props) => {
   };
 
   return (
-    <Card className="mt-5 mb-3">
-      <Card.Body>
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/explorers/${explorer_id}`}>
-            <Avatar src={explorer_image} height={55} />
-            {owner}
-          </Link>
-          <div className='d-flex align-items-center'>
-            <span>{updated_at}</span>
-            {is_owner && postDetail && (
-              <EditDeleteDropdown 
-                handleEdit={handleEdit} 
-                handleDelete={handleDelete} 
-              /> 
-            )}
-          </div>
-        </Media>
-      </Card.Body>
-      <Link to={`/posts/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
-      <Card.Body>
-        {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {description && <Card.Text className="text-center">{description}</Card.Text>}
-        {country && <Card.Text className="text-center"><i class="fa-solid fa-location-dot"></i>{country}</Card.Text>}
-        <div className={styles.Post}>
-          <Link to={`/posts/${id}`}>
-            <i className="far fa-comments" />
-          </Link>
-          {comments_count}
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't like your own post!</Tooltip>}
-            >
-              <i className="fa-regular fa-thumbs-up" />
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={handleUnlike}>
-              <i className="fa-solid fa-thumbs-up" />
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleLike}>
-              <i className="fa-regular fa-thumbs-up" />
-            </span>   
-          ) : (
-            <span>
+    <>
+      <Card className="mt-5 mb-3">
+        <Card.Body>
+          <Media className="align-items-center justify-content-between">
+            <Link to={`/explorers/${explorer_id}`}>
+              <Avatar src={explorer_image} height={55} />
+              {owner}
+            </Link>
+            <div className='d-flex align-items-center'>
+              <span>{updated_at}</span>
+              {is_owner && postDetail && (
+                <EditDeleteDropdown 
+                  handleEdit={handleEdit} 
+                  handleDelete={handleShow} 
+                /> 
+              )}
+            </div>
+          </Media>
+        </Card.Body>
+        <Link to={`/posts/${id}`}>
+          <Card.Img src={image} alt={title} />
+        </Link>
+        <Card.Body>
+          {title && <Card.Title className="text-center">{title}</Card.Title>}
+          {description && <Card.Text className="text-center">{description}</Card.Text>}
+          {country && <Card.Text className="text-center"><i class="fa-solid fa-location-dot"></i>{country}</Card.Text>}
+          <div className={styles.Post}>
+            <Link to={`/posts/${id}`}>
+              <i className="far fa-comments" />
+            </Link>
+            {comments_count}
+            {is_owner ? (
               <OverlayTrigger
                 placement="top"
-                overlay={<Tooltip>Log in to like posts!</Tooltip>}
+                overlay={<Tooltip>You can't like your own post!</Tooltip>}
               >
                 <i className="fa-regular fa-thumbs-up" />
               </OverlayTrigger>
-            </span>
-          )}
-          {likes_count}
-          {bookmark_id ? (
-            <span onClick={() => {}}>
-              <i className="fa-solid fa-bookmark" />
-            </span>
-          ) : currentUser ? (
-            <span onClick={() => {}}>
-              <i className="fa-regular fa-bookmark" />
-            </span>   
-          ) : (
-            <span>
-              <OverlayTrigger
-                placement="top"
-                overlay={<Tooltip>Log in to bookmark posts!</Tooltip>}
-              >
+            ) : like_id ? (
+              <span onClick={handleUnlike}>
+                <i className="fa-solid fa-thumbs-up" />
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleLike}>
+                <i className="fa-regular fa-thumbs-up" />
+              </span>   
+            ) : (
+              <span>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Log in to like posts!</Tooltip>}
+                >
+                  <i className="fa-regular fa-thumbs-up" />
+                </OverlayTrigger>
+              </span>
+            )}
+            {likes_count}
+            {bookmark_id ? (
+              <span onClick={() => {}}>
+                <i className="fa-solid fa-bookmark" />
+              </span>
+            ) : currentUser ? (
+              <span onClick={() => {}}>
                 <i className="fa-regular fa-bookmark" />
-              </OverlayTrigger>
-            </span>
-          )}
-          {bookmarks_count}
-        </div>
-      </Card.Body>
-    </Card>
+              </span>   
+            ) : (
+              <span>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={<Tooltip>Log in to bookmark posts!</Tooltip>}
+                >
+                  <i className="fa-regular fa-bookmark" />
+                </OverlayTrigger>
+              </span>
+            )}
+            {bookmarks_count}
+          </div>
+        </Card.Body>
+      </Card>
+      <ModalDeleteConfirmation 
+        showModal={show} 
+        handleClose = {handleClose} 
+        handleDelete = {handleDelete}
+      />
+    </>
   )
 }
 
