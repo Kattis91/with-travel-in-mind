505eb1615822254d830f3b3b636da08f9633d3ea
Kattis91 <katya.svedmark@gmail.com>
2023-11-16 14:51:42 +0000

    Add PopularProfiles comp and import it to the PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
 import { Form } from "react-bootstrap";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -79,7 +80,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Most adventurous explorers for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import appStyles from '../../App.module.css'
+import { Container } from 'react-bootstrap'
+
+const PopularProfiles = () => {
+
+  return (
+    <Container className={appStyles.Content}>
+      <p>Most adventurous explorers:</p>    
+    </Container>
+  );
+};
+
+export default PopularProfiles
