c2fd49e94f7c1d5e943ca2de38f4bcfa9eb880a9
Kattis91 <katya.svedmark@gmail.com>
2023-11-16 15:02:20 +0000

    Add logic to make request to API to fetch explorers

--- src/pages/profiles/PopularExplorers.js
+++ src/pages/profiles/PopularExplorers.js
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import appStyles from '../../App.module.css'
 import { Container } from 'react-bootstrap'
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { axiosReq } from '../../api/axiosDefaults';
 
 const PopularExplorers = () => {
+
+  const [explorerData, setExplorerData] = useState({
+    popularExplorers: { results: [] },
+  });
+  const { popularExplorers } = explorerData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/explorers/?ordering=-followers_count"
+        );
+        setExplorerData((prevState) => ({
+          ...prevState,
+          popularExplorers: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
 
   return (
     <Container className={appStyles.Content}>
