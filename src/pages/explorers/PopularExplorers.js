5dd00d53b0e126f8e187891c102369de3e35f19b
Kattis91 <katya.svedmark@gmail.com>
2023-11-16 19:05:07 +0000

    Adjust imports(remove and add)

--- src/pages/explorers/PopularExplorers.js
+++ src/pages/explorers/PopularExplorers.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import appStyles from '../../App.module.css'
 import { Container } from 'react-bootstrap'
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { axiosReq } from '../../api/axiosDefaults';
 import Asset from '../../components/Asset';
 import Explorer from './Explorer';
+import { useExplorerData } from '../../contexts/ExplorerDataContext';
 
 const PopularExplorers = ({ mobile }) => {
 
-  const [explorerData, setExplorerData] = useState({
-    popularExplorers: { results: [] },
-  });
-  const { popularExplorers } = explorerData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/explorers/?ordering=-followers_count"
-        );
-        setExplorerData((prevState) => ({
-          ...prevState,
-          popularExplorers: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { popularExplorers } = useExplorerData();
 
   return (
     <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
