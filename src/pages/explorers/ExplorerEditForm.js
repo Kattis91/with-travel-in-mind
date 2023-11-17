935db1edc21185fbd86d5137252bc02d015fb1fb
Kattis91 <katya.svedmark@gmail.com>
2023-11-17 20:58:57 +0000

    Add useEffect hook and handleSubmit function

--- src/pages/explorers/ExplorerEditForm.js
+++ src/pages/explorers/ExplorerEditForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Image from "react-bootstrap/Image";
@@ -9,11 +9,16 @@ import Container from "react-bootstrap/Container";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { Button } from "react-bootstrap";
-import { useHistory } from "react-router";
+import { useHistory, useParams } from "react-router";
+import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const ExplorerEditForm = () => {
-
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
   const history = useHistory();
+  const imageFile = useRef();
 
   const [explorerData, setExplorerData] = useState({
     name: '',
@@ -24,6 +29,49 @@ const ExplorerEditForm = () => {
   });
 
   const { name, bio, region_you_would_like_to_explore, dream_destination, image } = explorerData;
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser?.explorer_id?.toString() === id) {
+        try {
+          const { data } = await axiosReq.get(`/explorers/${id}/`);
+          const { name, bio, region_you_would_like_to_explore, dream_destination, image } = data;
+          setExplorerData({ name, bio, region_you_would_like_to_explore, dream_destination, image });
+        } catch (err) {
+          console.log(err);
+          history.push("/");
+        }
+      } else {
+        history.push("/");
+      }
+    };
+  
+    handleMount();
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("bio", bio);
+    formData.append("region_you_would_like_to_explore", region_you_would_like_to_explore);
+    formData.append("dream_destination", dream_destination);
+  
+    if (imageFile?.current?.files[0]) {
+      formData.append("image", imageFile?.current?.files[0]);
+    }
+    
+    try {
+      const { data } = await axiosReq.put(`/explorers/${id}/`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        explorer_image: data.image,
+      }));
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const textFields = (
     <>
@@ -56,7 +104,7 @@ const ExplorerEditForm = () => {
       <Form.Group>
         <Form.Label>Dream destination:</Form.Label>
         <Form.Control
-          type = "text"
+          type="text"
           value={dream_destination}
           name="dream_destination"
         />
@@ -74,7 +122,7 @@ const ExplorerEditForm = () => {
   );
  
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
           <Container className={appStyles.Content}>
