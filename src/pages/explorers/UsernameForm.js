ba9da4d7bd12c0ba855613e0711170ac1d97ace3
Kattis91 <katya.svedmark@gmail.com>
2023-11-18 11:47:02 +0000

    Add useEffect hook and handleSubmit function

--- src/pages/explorers/UsernameForm.js
+++ src/pages/explorers/UsernameForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -9,16 +9,47 @@ import Row from "react-bootstrap/Row";
 import { useHistory } from "react-router-dom";
 
 import appStyles from "../../App.module.css";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 const UsernameForm = () => {
 
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const [username, setUsername] = useState("");
+
   const history = useHistory();
+
+  useEffect(() => {
+    if (currentUser?.explorer_id?.toString() === id) {
+      setUsername(currentUser.username);
+    } else {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Row>
       <Col className="py-2 mx-auto text-center" md={6}>
         <Container className={appStyles.Content}>
-          <Form className="my-2">
+          <Form onSubmit={handleSubmit} className="my-2">
             <Form.Group>
               <Form.Label>Change username</Form.Label>
               <Form.Control
